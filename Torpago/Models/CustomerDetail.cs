50d15ee1cb973922346e575c61dfaeb0ee7ee035
Timothy Choi <TurokChoi@gmail.com>
2019-11-19 23:48:34 -0800

    Add project files.

---
+++ Torpago/Models/CustomerDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Torpago.Models
+{
+    public class CustomerDetail
+    {
+        [Key]
+        public int PMid { get; set; }
+
+        [Required]
+        [Column(TypeName ="nvarchar(100)")]
+        public string CustomerName { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public String State { get; set; }
+
+        [Required]
+        public int ZipCode { get; set; }
+    }
+}
